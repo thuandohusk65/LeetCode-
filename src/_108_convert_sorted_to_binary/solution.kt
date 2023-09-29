20582152e6185a6e48c0f2934dd46d4308468fc8
thuandd.keego <thuandohusk65@gmail.com>
2023-09-29 12:44:15 +0700

    108-convert sorted array to balance binary tree

---
+++ src/_108_convert_sorted_to_binary/solution.kt
@@ -0,0 +1,25 @@
+package _108_convert_sorted_to_binary
+
+import kotlin.math.max
+import kotlin.math.sign
+
+
+var ti = TreeNode(5)
+var v = ti.value
+
+class TreeNode(var value: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+fun sortedArrayToBST(nums: IntArray): TreeNode? {
+    if (nums.isEmpty()) return null
+    val middleIndex = max(0, nums.size / 2)
+    val middleValue = nums[middleIndex]
+    val node = TreeNode(middleValue)
+    if(middleIndex != 0) node.left = sortedArrayToBST(nums.take(middleIndex).toIntArray())
+    if(middleIndex < nums.size - 1) node.right = sortedArrayToBST(nums.takeLast(nums.size - middleIndex - 1).toIntArray())
+    return node
+}
+
+fun main() {}
